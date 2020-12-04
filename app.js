aae3c46a2327c7f734ea899e53be0b14821101d1
Koi <Makoi.Gacula@student.csulb.edu>
2020-12-03 20:41:46 -0800

    implemented gradient animation with an image mask on website logo

--- app.js
+++ app.js
@@ -67,3 +67,22 @@ const hideMobileMenu = () => {
 // adds event listeners
 menuLinks.addEventListener("click", hideMobileMenu);
 navLogo.addEventListener("click", hideMobileMenu);
+
+// For gradient Animation
+var granimInstance = new Granim({
+  element: "#logo-canvas",
+  direction: "left-right",
+  states: {
+    "default-state": {
+      gradients: [
+        ["#EB3349", "#F45C43"],
+        ["#FF8008", "#FFC837"],
+        ["#4CB8C4", "#3CD3AD"],
+        ["#24C6DC", "#514A9D"],
+        ["#FF512F", "#DD2476"],
+        ["#DA22FF", "#9733EE"],
+      ],
+      transitionSpeed: 2000,
+    },
+  },
+});
